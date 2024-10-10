893514133a69360136a1b4879e2919e4237274a8
bftsgt <bfutasz@gmail.com>
2024-10-10 19:11:21 +0200

    prompt-sync added

--- client.js
+++ client.js
@@ -1,29 +1,35 @@
 const prompt = require("prompt-sync")();
 
 
-const filterName = prompt("Enter the name to filter users: ");
-fetch(`http://localhost:3000/filterUsersName?name=${filterName}`)
+fetch("http://localhost:3000/todos")
   .then((res) => res.json())
-  .then((data) => console.log("Filtered users: ", data))
+  .then((data) => console.log("List of Todos: ", data))
   .catch((err) => console.error(err));
 
 
-const newName = prompt("Enter the name of the new user: ");
-fetch("http://localhost:3000/addUser", {
+const filterName = prompt("Enter the name to filter todos: ");
+fetch(`http://localhost:3000/filterTodos?name=${filterName}`)
+  .then((res) => res.json())
+  .then((data) => console.log("Filtered todos: ", data))
+  .catch((err) => console.error(err));
+
+
+const newTodo = prompt("Enter the new todo: ");
+fetch("http://localhost:3000/addTodo", {
   method: "POST",
   headers: {
     "Content-Type": "application/json",
   },
-  body: JSON.stringify({ name: newName }),
+  body: JSON.stringify({ name: newTodo }),
 })
   .then((res) => res.json())
-  .then((data) => console.log("New user added: ", data))
+  .then((data) => console.log("New todo added: ", data))
   .catch((err) => console.error(err));
 
 
-const updateId = prompt("Enter the ID of the user to update: ");
-const updateName = prompt("Enter the new name for the user: ");
-fetch(`http://localhost:3000/updateUser/${updateId}`, {
+const updateId = prompt("Enter the ID of the todo to update: ");
+const updateName = prompt("Enter the new name for the todo: ");
+fetch(`http://localhost:3000/updateTodo/${updateId}`, {
   method: "PUT",
   headers: {
     "Content-Type": "application/json",
@@ -31,12 +37,12 @@ fetch(`http://localhost:3000/updateUser/${updateId}`, {
   body: JSON.stringify({ name: updateName }),
 })
   .then((res) => res.json())
-  .then((data) => console.log("User updated: ", data))
+  .then((data) => console.log("Todo updated: ", data))
   .catch((err) => console.error(err));
 
 
-const deleteId = prompt("Enter the ID of the user to delete: ");
-fetch(`http://localhost:3000/deleteUser/${deleteId}`, {
+const deleteId = prompt("Enter the ID of the todo to delete: ");
+fetch(`http://localhost:3000/deleteTodo/${deleteId}`, {
   method: "DELETE",
 })
   .then((res) => res.json())
